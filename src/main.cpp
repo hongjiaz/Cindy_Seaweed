dce9feed8e7b4fad25be2bbde248e61771401d07
Hongjia Zhang <hongjia.zhang.gwst@gmail.com>
2015-05-12 17:41:00 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,26 +13,38 @@ ofPolyline polyline;
 
 
 void setup() {
-    ofSetWindowShape(600, 600);    // the window size
-    for (int i=200; i>=-180; i-=9) polyline.addVertex(ofVec2f(0,i));    // set the location 
+    ofSetWindowShape(600, 600);
+    // the window size
+    for (int i=200; i>=-180; i-=9) polyline.addVertex(ofVec2f(0,i));
+    // set the location 
     movePolyline = polyline;
+    //set the polyline
 }
 
 
 void draw() {
-    ofBackground(229);    // set the background color  
+    ofBackground(229);    
+    // set the background color  
       for (int i=0; i<polyline.size(); i++) {
+      //set the whole number
         ofVec3f normal = polyline.getNormalAtIndexInterpolated(i); 
-        ofPushMatrix();    //set the basic looking of the grass
+        ofPushMatrix();    
+        //set the basic looking of the grass
             ofTranslate(600/2, 600/2);
+            //set the translate
             ofLine(movePolyline[i]-normal*(polyline.size() -i), movePolyline[i]+normal*(polyline.size() -i));
         ofPopMatrix();
+        //push .. pop matrix
         ofPushMatrix();
             ofTranslate(600/2, 600/2);
-            movePolyline.draw();        
+            //set the translate
+            movePolyline.draw(); 
+            //draw the polyline
         ofPopMatrix();
+        //push .. pop matrix
     }
-    ofSetColor(0,153,77);    // the color of the grass
+    ofSetColor(0,153,77);    
+    // the color of the grass
 }
 
 };
